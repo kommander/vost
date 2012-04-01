3018745737d9ef6583b41fcd93bffa6f67408e8d
Sebastian Herrlinger <sebastian@formzoo.com>
2012-04-01 22:53:20 +0200

    setup multiple servers at several ports for testing

--- dev/receiver.js
+++ dev/receiver.js
@@ -1,8 +1,33 @@
 var http = require('http')
+var util = require('util')
 
-// Setup Server
-var server = http.createServer(function(req, res) {
-  console.log('Received request for: ', req.headers.host, req.url);
-  res.end('Here I am. (' + req.url + ')');
+//Take command line arguments
+var settings = {
+	ports: [4040]
+};
+
+process.argv.forEach(function (val, index, array) {
+  if(index < 2)
+    return;
+  var valArr = val.split('=');
+  switch(valArr[0]){
+    case 'ports':
+        settings.ports = valArr[1].split(',');
+      break;
+    default:
+      util.print('Argument unknown or malformed: ' + val + '\nStopping process.');
+      process.exit();
+  }
 });
-server.listen(4040);
+
+// Setup Server Instances
+for(var i = 0; i < settings.ports.length; i++){
+  var port = parseInt(settings.ports[i], 10);
+  var server = http.createServer((function(port){
+    return function(req, res) {
+	    console.log('Received request for: ', req.headers.host, req.url, '@', port);
+	    res.end('Here I am. (' + req.headers.host + req.url + ' @ ' + port + ')');
+    };
+	})(port));
+	server.listen(port);	
+}
